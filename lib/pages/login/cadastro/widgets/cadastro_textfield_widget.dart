0397521be18ce6187663bd656df89f3f1235855c
Luigi Trevisan <checkzin123@gmail.com>
2022-08-04 23:20:58 -0300

    Bug Fix da mensagem de erro e keyboardType adicionado

--- lib/pages/login/cadastro/widgets/cadastro_textfield_widget.dart
+++ lib/pages/login/cadastro/widgets/cadastro_textfield_widget.dart
@@ -88,6 +88,13 @@ class CadastroTextFieldWidget extends StatelessWidget {
                             }
                           }
                         : null;
+    var keyboardType = tipoCampoTextoEnum == TipoCampoTextoEnum.RA
+        ? TextInputType.number
+        : tipoCampoTextoEnum == TipoCampoTextoEnum.EMAIL
+            ? TextInputType.emailAddress
+            : tipoCampoTextoEnum == TipoCampoTextoEnum.TELEFONE
+                ? TextInputType.phone
+                : TextInputType.text;
     var nullMask = MaskTextInputFormatter();
     var maskFormatter = tipoCampoTextoEnum == TipoCampoTextoEnum.RA
         ? MaskTextInputFormatter(
@@ -113,10 +120,10 @@ class CadastroTextFieldWidget extends StatelessWidget {
         enabled: isEnabled,
         onEditingComplete: () => FocusScope.of(context).nextFocus(),
         obscureText: isObscure,
+        keyboardType: keyboardType,
         style: const TextStyle(fontSize: 16),
         decoration: InputDecoration(
             counterText: '',
-            errorStyle: TextStyle(fontSize: 0),
             errorBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide: BorderSide(
